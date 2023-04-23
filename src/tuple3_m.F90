bf01d7d9d865f59c92b51a1b0faa0d8e5f0563e2
osada-yum <osada.yum@gmail.com>
2023-04-24 01:15:22 +0900

    Add tuple2, tuple3 and priority_queue for tuple3.

---
+++ src/tuple3_m.F90
@@ -0,0 +1,225 @@
+module tuple3_m
+  use, intrinsic :: iso_fortran_env
+  implicit none
+  public :: tuple3_int32_int32_int32
+  type :: tuple3_int32_int32_int32
+     private
+     integer(int32) :: fst_
+     integer(int32) :: snd_
+     integer(int32) :: thr_
+   contains
+     procedure, pass :: fst  => fst_tuple3_int32_int32_int32
+     procedure, pass :: snd  => snd_tuple3_int32_int32_int32
+     procedure, pass :: thr  => thr_tuple3_int32_int32_int32
+  end type tuple3_int32_int32_int32
+  public :: construct_tuple3_int32_int32_int32
+  interface tuple3_int32_int32_int32
+     module procedure :: construct_tuple3_int32_int32_int32
+  end interface tuple3_int32_int32_int32
+  interface operator(<)
+     module procedure :: less_tuple3_int32_int32_int32
+  end interface operator(<)
+  interface operator(<=)
+     module procedure :: less_equal_tuple3_int32_int32_int32
+  end interface operator(<=)
+  interface operator(>)
+     module procedure :: greater_tuple3_int32_int32_int32
+  end interface operator(>)
+  interface operator(>=)
+     module procedure :: greater_equal_tuple3_int32_int32_int32
+  end interface operator(>=)
+  interface operator(==)
+     module procedure :: equal_tuple3_int32_int32_int32
+  end interface operator(==)
+  interface operator(/=)
+     module procedure :: not_equal_tuple3_int32_int32_int32
+  end interface operator(/=)
+  
+  public :: tuple3_int64_int64_int64
+  type :: tuple3_int64_int64_int64
+     private
+     integer(int64) :: fst_
+     integer(int64) :: snd_
+     integer(int64) :: thr_
+   contains
+     procedure, pass :: fst  => fst_tuple3_int64_int64_int64
+     procedure, pass :: snd  => snd_tuple3_int64_int64_int64
+     procedure, pass :: thr  => thr_tuple3_int64_int64_int64
+  end type tuple3_int64_int64_int64
+  public :: construct_tuple3_int64_int64_int64
+  interface tuple3_int64_int64_int64
+     module procedure :: construct_tuple3_int64_int64_int64
+  end interface tuple3_int64_int64_int64
+  interface operator(<)
+     module procedure :: less_tuple3_int64_int64_int64
+  end interface operator(<)
+  interface operator(<=)
+     module procedure :: less_equal_tuple3_int64_int64_int64
+  end interface operator(<=)
+  interface operator(>)
+     module procedure :: greater_tuple3_int64_int64_int64
+  end interface operator(>)
+  interface operator(>=)
+     module procedure :: greater_equal_tuple3_int64_int64_int64
+  end interface operator(>=)
+  interface operator(==)
+     module procedure :: equal_tuple3_int64_int64_int64
+  end interface operator(==)
+  interface operator(/=)
+     module procedure :: not_equal_tuple3_int64_int64_int64
+  end interface operator(/=)
+  
+contains
+  !> construct_tuple3_int32_int32_int32_by_size: Construct tuple3_int32_int32_int32.
+  impure function construct_tuple3_int32_int32_int32(val1, val2, val3) result(res)
+    type(tuple3_int32_int32_int32) :: res
+    integer(int32), intent(in) :: val1
+    integer(int32), intent(in) :: val2
+    integer(int32), intent(in) :: val3
+    res%fst_ = val1
+    res%snd_ = val2
+    res%thr_ = val3
+  end function construct_tuple3_int32_int32_int32
+  !> fst_tuple3_int32_int32_int32: Return the first element of tuple3_int32_int32_int32.
+  integer(int32) function fst_tuple3_int32_int32_int32(this) result(res)
+    class(tuple3_int32_int32_int32), intent(in) :: this
+    res = this%fst_
+  end function fst_tuple3_int32_int32_int32
+  !> snd_tuple3_int32_int32_int32: Return the second element of tuple3_int32_int32_int32.
+  integer(int32) function snd_tuple3_int32_int32_int32(this) result(res)
+    class(tuple3_int32_int32_int32), intent(in) :: this
+    res = this%snd_
+  end function snd_tuple3_int32_int32_int32
+  !> thr_tuple3_int32_int32_int32: Return the second element of tuple3_int32_int32_int32.
+  integer(int32) function thr_tuple3_int32_int32_int32(this) result(res)
+    class(tuple3_int32_int32_int32), intent(in) :: this
+    res = this%thr_
+  end function thr_tuple3_int32_int32_int32
+  !> less_tuple3_int32_int32_int32: Compare the first elements.
+  !> Compare the second elements if the first elements are same.
+  logical function less_tuple3_int32_int32_int32(lhs, rhs) result(res)
+    type(tuple3_int32_int32_int32), intent(in) :: lhs, rhs
+    res = lhs%fst_ < rhs%fst_
+    if (lhs%fst_ == rhs%fst_) then
+       res = lhs%snd_ < rhs%snd_
+       if (lhs%snd_ == rhs%snd_) then
+          res = lhs%thr_ < rhs%thr_
+       end if
+    end if
+  end function less_tuple3_int32_int32_int32
+  logical function less_equal_tuple3_int32_int32_int32(lhs, rhs) result(res)
+    type(tuple3_int32_int32_int32), intent(in) :: lhs, rhs
+    res = lhs%fst_ < rhs%fst_
+    if (lhs%fst_ == rhs%fst_) then
+       res = lhs%snd_ < rhs%snd_
+       if (lhs%snd_ == rhs%snd_) then
+          res = lhs%thr_ <= rhs%thr_
+       end if
+    end if
+  end function less_equal_tuple3_int32_int32_int32
+  logical function greater_tuple3_int32_int32_int32(lhs, rhs) result(res)
+    type(tuple3_int32_int32_int32), intent(in) :: lhs, rhs
+    res = lhs%fst_ > rhs%fst_
+    if (lhs%fst_ == rhs%fst_) then
+       res = lhs%snd_ > rhs%snd_
+       if (lhs%snd_ == rhs%snd_) then
+          res = lhs%thr_ > rhs%thr_
+       end if
+    end if
+  end function greater_tuple3_int32_int32_int32
+  logical function greater_equal_tuple3_int32_int32_int32(lhs, rhs) result(res)
+    type(tuple3_int32_int32_int32), intent(in) :: lhs, rhs
+    res = lhs%fst_ > rhs%fst_
+    if (lhs%fst_ == rhs%fst_) then
+       res = lhs%snd_ > rhs%snd_
+       if (lhs%snd_ == rhs%snd_) then
+          res = lhs%thr_ >= rhs%thr_
+       end if
+    end if
+  end function greater_equal_tuple3_int32_int32_int32
+  logical function equal_tuple3_int32_int32_int32(lhs, rhs) result(res)
+    type(tuple3_int32_int32_int32), intent(in) :: lhs, rhs
+    res = lhs%fst_ == rhs%fst_ .and. lhs%snd_ == rhs%snd_ .and. lhs%thr_ == rhs%thr_
+  end function equal_tuple3_int32_int32_int32
+  logical function not_equal_tuple3_int32_int32_int32(lhs, rhs) result(res)
+    type(tuple3_int32_int32_int32), intent(in) :: lhs, rhs
+    res = lhs%fst_ /= rhs%fst_ .or. lhs%snd_ /= rhs%snd_ .or. lhs%thr_ /= rhs%thr_
+  end function not_equal_tuple3_int32_int32_int32
+  
+  !> construct_tuple3_int64_int64_int64_by_size: Construct tuple3_int64_int64_int64.
+  impure function construct_tuple3_int64_int64_int64(val1, val2, val3) result(res)
+    type(tuple3_int64_int64_int64) :: res
+    integer(int64), intent(in) :: val1
+    integer(int64), intent(in) :: val2
+    integer(int64), intent(in) :: val3
+    res%fst_ = val1
+    res%snd_ = val2
+    res%thr_ = val3
+  end function construct_tuple3_int64_int64_int64
+  !> fst_tuple3_int64_int64_int64: Return the first element of tuple3_int64_int64_int64.
+  integer(int64) function fst_tuple3_int64_int64_int64(this) result(res)
+    class(tuple3_int64_int64_int64), intent(in) :: this
+    res = this%fst_
+  end function fst_tuple3_int64_int64_int64
+  !> snd_tuple3_int64_int64_int64: Return the second element of tuple3_int64_int64_int64.
+  integer(int64) function snd_tuple3_int64_int64_int64(this) result(res)
+    class(tuple3_int64_int64_int64), intent(in) :: this
+    res = this%snd_
+  end function snd_tuple3_int64_int64_int64
+  !> thr_tuple3_int64_int64_int64: Return the second element of tuple3_int64_int64_int64.
+  integer(int64) function thr_tuple3_int64_int64_int64(this) result(res)
+    class(tuple3_int64_int64_int64), intent(in) :: this
+    res = this%thr_
+  end function thr_tuple3_int64_int64_int64
+  !> less_tuple3_int64_int64_int64: Compare the first elements.
+  !> Compare the second elements if the first elements are same.
+  logical function less_tuple3_int64_int64_int64(lhs, rhs) result(res)
+    type(tuple3_int64_int64_int64), intent(in) :: lhs, rhs
+    res = lhs%fst_ < rhs%fst_
+    if (lhs%fst_ == rhs%fst_) then
+       res = lhs%snd_ < rhs%snd_
+       if (lhs%snd_ == rhs%snd_) then
+          res = lhs%thr_ < rhs%thr_
+       end if
+    end if
+  end function less_tuple3_int64_int64_int64
+  logical function less_equal_tuple3_int64_int64_int64(lhs, rhs) result(res)
+    type(tuple3_int64_int64_int64), intent(in) :: lhs, rhs
+    res = lhs%fst_ < rhs%fst_
+    if (lhs%fst_ == rhs%fst_) then
+       res = lhs%snd_ < rhs%snd_
+       if (lhs%snd_ == rhs%snd_) then
+          res = lhs%thr_ <= rhs%thr_
+       end if
+    end if
+  end function less_equal_tuple3_int64_int64_int64
+  logical function greater_tuple3_int64_int64_int64(lhs, rhs) result(res)
+    type(tuple3_int64_int64_int64), intent(in) :: lhs, rhs
+    res = lhs%fst_ > rhs%fst_
+    if (lhs%fst_ == rhs%fst_) then
+       res = lhs%snd_ > rhs%snd_
+       if (lhs%snd_ == rhs%snd_) then
+          res = lhs%thr_ > rhs%thr_
+       end if
+    end if
+  end function greater_tuple3_int64_int64_int64
+  logical function greater_equal_tuple3_int64_int64_int64(lhs, rhs) result(res)
+    type(tuple3_int64_int64_int64), intent(in) :: lhs, rhs
+    res = lhs%fst_ > rhs%fst_
+    if (lhs%fst_ == rhs%fst_) then
+       res = lhs%snd_ > rhs%snd_
+       if (lhs%snd_ == rhs%snd_) then
+          res = lhs%thr_ >= rhs%thr_
+       end if
+    end if
+  end function greater_equal_tuple3_int64_int64_int64
+  logical function equal_tuple3_int64_int64_int64(lhs, rhs) result(res)
+    type(tuple3_int64_int64_int64), intent(in) :: lhs, rhs
+    res = lhs%fst_ == rhs%fst_ .and. lhs%snd_ == rhs%snd_ .and. lhs%thr_ == rhs%thr_
+  end function equal_tuple3_int64_int64_int64
+  logical function not_equal_tuple3_int64_int64_int64(lhs, rhs) result(res)
+    type(tuple3_int64_int64_int64), intent(in) :: lhs, rhs
+    res = lhs%fst_ /= rhs%fst_ .or. lhs%snd_ /= rhs%snd_ .or. lhs%thr_ /= rhs%thr_
+  end function not_equal_tuple3_int64_int64_int64
+  
+end module tuple3_m
